36eead5b0bc45a304b1760bde9fb929be2531615
IHMHR <martinelli.igor@hotmail.com>
2016-04-11 00:58:10 -0300

    Atualizando GitHub
    Erro com a conexão com a Base de Dados..

--- SeriesSolution/SeriesSolution/Form1.Designer.cs
+++ SeriesSolution/SeriesSolution/Form1.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lojaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.seriesDataSet1 = new SeriesSolution.seriesDataSet1();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.seriadoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.seriesDataSet = new SeriesSolution.seriesDataSet();
+            this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -46,11 +56,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.seriadoTableAdapter = new SeriesSolution.seriesDataSetTableAdapters.seriadoTableAdapter();
+            this.lojaTableAdapter = new SeriesSolution.seriesDataSet1TableAdapters.lojaTableAdapter();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lojaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriadoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label9);
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.textBox6);
             this.groupBox1.Controls.Add(this.label7);
@@ -68,15 +89,87 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(372, 158);
+            this.groupBox1.Size = new System.Drawing.Size(380, 217);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Visitas";
             // 
+            // comboBox2
+            // 
+            this.comboBox2.DataSource = this.lojaBindingSource;
+            this.comboBox2.DisplayMember = "loja";
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(88, 173);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(272, 21);
+            this.comboBox2.TabIndex = 19;
+            this.comboBox2.ValueMember = "id";
+            // 
+            // lojaBindingSource
+            // 
+            this.lojaBindingSource.DataMember = "loja";
+            this.lojaBindingSource.DataSource = this.seriesDataSet1;
+            // 
+            // seriesDataSet1
+            // 
+            this.seriesDataSet1.DataSetName = "seriesDataSet1";
+            this.seriesDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Loja Visitada:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 22);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Agora";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.seriadoBindingSource;
+            this.comboBox1.DisplayMember = "seriado";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(63, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(297, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.ValueMember = "id";
+            // 
+            // seriadoBindingSource
+            // 
+            this.seriadoBindingSource.DataMember = "seriado";
+            this.seriadoBindingSource.DataSource = this.seriesDataSet;
+            // 
+            // seriesDataSet
+            // 
+            this.seriesDataSet.DataSetName = "seriesDataSet";
+            this.seriesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Seriado:";
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(172, 98);
+            this.label8.Location = new System.Drawing.Point(172, 124);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(35, 13);
             this.label8.TabIndex = 14;
@@ -84,7 +177,7 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(85, 121);
+            this.textBox6.Location = new System.Drawing.Point(85, 147);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(275, 20);
             this.textBox6.TabIndex = 13;
@@ -92,7 +185,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 124);
+            this.label7.Location = new System.Drawing.Point(12, 150);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(67, 13);
             this.label7.TabIndex = 12;
@@ -101,7 +194,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 98);
+            this.label6.Location = new System.Drawing.Point(12, 124);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(109, 13);
             this.label6.TabIndex = 11;
@@ -109,24 +202,26 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(127, 95);
+            this.textBox5.Location = new System.Drawing.Point(127, 121);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(39, 20);
             this.textBox5.TabIndex = 10;
             this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
             this.textBox5.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox5_KeyUp);
+            this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(252, 69);
+            this.textBox4.Location = new System.Drawing.Point(252, 95);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(108, 20);
             this.textBox4.TabIndex = 9;
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(169, 72);
+            this.label5.Location = new System.Drawing.Point(169, 98);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 13);
             this.label5.TabIndex = 8;
@@ -134,16 +229,16 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(79, 69);
+            this.textBox3.Location = new System.Drawing.Point(79, 95);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(79, 20);
             this.textBox3.TabIndex = 7;
-            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 72);
+            this.label4.Location = new System.Drawing.Point(12, 98);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 6;
@@ -151,16 +246,16 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(332, 43);
+            this.textBox2.Location = new System.Drawing.Point(332, 69);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(28, 20);
             this.textBox2.TabIndex = 5;
-            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(221, 46);
+            this.label3.Location = new System.Drawing.Point(221, 72);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(105, 13);
             this.label3.TabIndex = 4;
@@ -168,16 +263,17 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 43);
+            this.textBox1.Location = new System.Drawing.Point(94, 69);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(108, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Location = new System.Drawing.Point(12, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 2;
@@ -203,7 +299,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 164);
+            this.button1.Location = new System.Drawing.Point(10, 223);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 0;
@@ -213,7 +309,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(263, 164);
+            this.button2.Location = new System.Drawing.Point(263, 223);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(109, 23);
             this.button2.TabIndex = 1;
@@ -221,11 +317,19 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // seriadoTableAdapter
+            // 
+            this.seriadoTableAdapter.ClearBeforeFill = true;
+            // 
+            // lojaTableAdapter
+            // 
+            this.lojaTableAdapter.ClearBeforeFill = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 199);
+            this.ClientSize = new System.Drawing.Size(389, 253);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -234,6 +338,10 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lojaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriadoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -258,6 +366,17 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private seriesDataSet seriesDataSet;
+        private System.Windows.Forms.BindingSource seriadoBindingSource;
+        private seriesDataSetTableAdapters.seriadoTableAdapter seriadoTableAdapter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label10;
+        private seriesDataSet1 seriesDataSet1;
+        private System.Windows.Forms.BindingSource lojaBindingSource;
+        private seriesDataSet1TableAdapters.lojaTableAdapter lojaTableAdapter;
     }
 }
 
--- SeriesSolution/SeriesSolution/Form1.cs
+++ SeriesSolution/SeriesSolution/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -13,6 +14,8 @@ namespace SeriesSolution
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.lojaTableAdapter.Fill(this.seriesDataSet1.loja);
+            this.seriadoTableAdapter.Fill(this.seriesDataSet.seriado);
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm:ss";
 
@@ -23,29 +26,22 @@ namespace SeriesSolution
             textBox5.MaxLength = 2;
 
             label8.Visible = false;
+
+            try
+            {
+                this.seriadoTableAdapter.FillBy(this.seriesDataSet.seriado);
+                this.lojaTableAdapter.FillBy(this.seriesDataSet1.loja);
+      
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar == '.')
-            {
-                e.Handled = true;
-            }
             
-            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
-            {
-                e.Handled = true;
-            }
-
-            if(textBox1.Text.Contains(',')) 
-            {
-                e.Handled = true;
-            }
-
-            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || !char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-            }
         }
 
         private void textBox5_KeyUp(object sender, KeyEventArgs e)
@@ -62,38 +58,54 @@ namespace SeriesSolution
             }
         }
 
-        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-            }
-        }
-
-        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar == '.')
-            {
-                e.Handled = true;
-            }
-
-            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
-            {
-                e.Handled = true;
-            }
-
-            if (textBox1.Text.Contains(','))
-            {
-                e.Handled = true;
-            }
-
-            if(char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || !char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.ForeColor.Equals(Color.Red) && textBox2.ForeColor.Equals(Color.Red) && textBox3.ForeColor.Equals(Color.Red) && textBox4.ForeColor.Equals(Color.Red) && textBox5.ForeColor.Equals(Color.Red))
+                MessageBox.Show("Existe algum campo que não foi preenchido", "Preencher todos os campos");
+            else
+            {
+                using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(@"Data Source=MARTINELLI-07\SQLEXPRESS;Initial Catalog=series;Integrated Security=True"))
+                {
+                    try
+                    {
+                        CampoVazio();
+
+                        System.Text.StringBuilder sb = new System.Text.StringBuilder("EXECUTE usp_InserirVisita ");
+                        sb.Append("'" + dateTimePicker1.Text + "',");
+                        sb.Append(textBox1.Text.Replace(",",".") + ",");
+                        sb.Append(textBox2.Text + ",");
+                        sb.Append(textBox3.Text.Replace(",", ".") + ",");
+                        sb.Append("'" + textBox4.Text + "',");
+                        sb.Append(textBox5.Text + ",");
+                        sb.Append("'" + textBox6.Text + "',");
+                        sb.Append(comboBox2.SelectedValue.ToString() + ",");
+                        sb.Append(comboBox1.SelectedValue.ToString() + ";");
+                        //Clipboard.SetText(sb.ToString());
+                        string sql = sb.ToString();
+                        Clipboard.SetText(sql);
+                        System.Data.SqlClient.SqlCommand cm = new System.Data.SqlClient.SqlCommand(sql, con);
+                        cm.CommandType = System.Data.CommandType.StoredProcedure;
+                        con.Open();
+                        cm.ExecuteNonQuery();
+
+                        MessageBox.Show("Salvo com sucesso!");
+                        ClearFields();
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void ClearFields()
         {
             textBox1.Clear();
             textBox2.Clear();
@@ -106,19 +118,71 @@ namespace SeriesSolution
             label8.Visible = false;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void CampoVazio()
         {
-            using(System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(@"Data Source=MARTINELLI-07\SQLEXPRESS;Initial Catalog=SS;Integrated Security=True"))
+            bool[] vazio = new bool[6];
+            vazio[0] = textBox1.Text.Equals(string.Empty) ? true : false;
+            vazio[1] = textBox2.Text.Equals(string.Empty) ? true : false;
+            vazio[2] = textBox3.Text.Equals(string.Empty) ? true : false;
+            vazio[3] = textBox4.Text.Equals(string.Empty) ? true : false;
+            vazio[4] = textBox5.Text.Equals(string.Empty) ? true : false;
+            vazio[5] = textBox6.Text.Equals(string.Empty) ? true : false;
+
+            bool msg = false;
+            for (int i = 0; i < vazio.Length; i++)
             {
-                try 
-	            {	        
-	            	con.Open();
-	            }
-	            catch (Exception ex)
-	            {
-	            	MessageBox.Show(ex.Message.ToString());
-	            }
+                if (vazio[i] == true)
+                    msg = true;
             }
+
+            if (msg)
+                MessageBox.Show("Existe algum campo que não foi preenchido", "Preencher todos os campos");
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, @"[0-9]*,[0-9]{2}"))
+                textBox1.ForeColor = Color.Red;
+            else
+                textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox2.Text, @"[1-9]{1,2}"))
+                textBox2.ForeColor = Color.Red;
+            else
+                textBox2.ForeColor = Color.Black;  
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox3.Text, @"[0-9]*,[0-9]{2}"))
+                textBox3.ForeColor = Color.Red;
+            else       
+                textBox3.ForeColor = Color.Black;
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, @".+[a-zA-z]+.+"))
+                textBox4.ForeColor = Color.Red;
+            else
+                textBox4.ForeColor = Color.Black;
+        }
+
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox5.Text, @"[0-9]{1,2}"))
+                textBox5.ForeColor = Color.Red;
+            else
+                textBox5.ForeColor = Color.Black;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now;
         }
     }
 }
